08c433ebc2f415828697fa0fedce6574d45f27f3
mariaeduarda-nog <maria.e.nogueira6@edu.senai.br>
2026-05-06 17:09:16 -0300

    first commit

---
+++ AtividadeEntrega-main/js/scripts.js
@@ -0,0 +1,41 @@
+// ATIVIDADE 01
+const ligar = () => {
+let lampada = document.getElementById("lampada");
+
+if(lampada.src.includes("apagada.jpg")) {
+    lampada.src = "img/acesa.jpg";
+} else {
+    lampada.src = "img/apagada.jpg";
+}
+
+};
+// -----------------------------------------
+
+// ATIVIDADE 02
+const gerarImagem = () => {
+let qtdImagem = Number(document.getElementById(inQtdImg).value);
+let canvas = document.getElementById("canvas");
+canvas.innerHTML = "";
+
+if (qtdImagem <= 0) {
+    alert("O campo quantidade não pode estar nulo ou negativo");
+} else {
+    for (let i = 1; i <= inQtdImagem; i++); {
+        canvas.innerHTML += <img id="logo" src="img/logo.png"
+        alta="Logotipo SENAI" class="imagens" />;
+    }
+}
+
+};
+// -----------------------------------------
+
+//ATIVIDADE 03
+const validaForm = () => {
+  
+};
+// -----------------------------------------
+
+// ATIVIDADE 04
+const calcular = () => {
+ 
+};
